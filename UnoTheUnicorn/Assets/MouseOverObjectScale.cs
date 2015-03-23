5f38a832d7d50f249dc536be395a8ef221e0f55c
Amanda Krause <amandakrause@Amanda-Krauses-iMac.local>
2015-03-23 20:52:10 +0100

    Skapat script som får objekt man kan plocka upp att skala upp när man "hoovrar" med musen över dem.

---
+++ UnoTheUnicorn/Assets/MouseOverObjectScale.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseOverObjectScale : MonoBehaviour {
+	public Renderer scaleBridgePart;
+	public Vector3 scaleVector;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseEnter(){
+		Scale(true);
+	}
+	void OnMouseExit(){
+		Scale(false);
+	}
+	void Scale(bool on)
+	{
+		if(on)
+		{
+			Debug.Log("Enter");
+			transform.localScale += new Vector3(0.5f,0.5f,0.5f);
+
+		}
+		else
+		{
+			Debug.Log("Exit");
+			transform.localScale -= new Vector3(0.5f,0.5f,0.5f);
+			
+		}
+}
+}
